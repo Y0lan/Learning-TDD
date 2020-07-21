cee291799341eba719b3ea7228ac8816096323d9
no_name <no@mail>
2020-07-22 01:09:26 +0200

    1

--- app.test.py
+++ app.test.py
@@ -1,14 +1,14 @@
 import os
-import unittest
 import tempfile
-import src.main as main
+import unittest
+import app
 
 
 class TestingBasicOfWebsite(unittest.TestCase):
     def test_index(self):
-        tester = main.main.test_client(self)
+        tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_database_exist(self):
         tester = os.path.exists('database.db')
@@ -16,60 +16,61 @@ class TestingBasicOfWebsite(unittest.TestCase):
 
 
 class FlaskrTestCase(unittest.TestCase):
-    def canSetupDatabase(self):
-        self.db_fd, main.main.config['DATABASE'] = tempfile.mkstemp()
-        main.main.config['TESTING'] = True
-        self.main = main.main.test_client()
-        main.init_db()
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
 
-    def canCloseDatabase(self):
+    def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(main.main.config['DATABASE'])
+        os.unlink(app.app.config['DATABASE'])
 
     def login(self, username, password):
-        return self.main.post('/login', data=dict(
+        return self.app.post('/login', data=dict(
             username=username,
             password=password
         ), follow_redirects=True)
 
     def logout(self):
-        return self.main.get('/logout', follow_redirects=True)
+        return self.app.get('/logout', follow_redirects=True)
 
-    def dbIsEmpty(self):
-        return self.main.get('/logout', follow_redirects=True)
+    def testDbIsEmpty(self):
+        response = self.app.get('/')
+        assert b'Aucun article pour le moment' in response.data
 
-    def canLoginWithCorrectCredentials(self):
+    def testCanLoginWithCorrectCredentials(self):
         response = self.login(
-            main.main.config['USERNAME'],
-            main.main.config['PASSWORD']
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD']
         )
         assert b'logged in' in response.data
         return response
 
-    def canInputLogout(self):
+    def testCanInputLogout(self):
         response = self.logout()
         assert b'logged out' in response.data
 
-    def cannotLoginWithIncorrectCrendentials(self):
+    def testCannotLoginWithIncorrectCrendentials(self):
         response = self.login(
-            main.main.config['USERNAME'] + 'thisistofalsetheusername',
-            main.main.config['PASSWORD']
+            app.app.config['USERNAME'] + 'thisistofalsetheusername',
+            app.app.config['PASSWORD']
         )
         assert b'Invalid username' in response.data
 
         response = self.login(
-            main.main.config['USERNAME'],
-            main.main.config['PASSWORD'] + 'thisistofalsethepassword'
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD'] + 'thisistofalsethepassword'
         )
         assert b'Invalid password' in response.data
 
-    def canPostArticleWhenLoggedIn(self):
+    def testCanPostArticleWhenLoggedIn(self):
         self.login(
-            main.main.config['USERNAME'],
-            main.main.config['PASSWORD']
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD']
         )
 
-        response = self.main.post('/add', data=dict(
+        response = self.app.post('/add', data=dict(
             titre='<Hello World>',
             contenu='<strong> Ce texte est du HTML ! :-)</strong>'
         ), follow_redirects=True)
