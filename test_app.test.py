d3e5b838f6e573641d1af84c03ff3885e5ce7788
no_name <no@mail>
2020-07-22 13:40:08 +0200

    initial

---
+++ test_app.test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_flaskr_himtest_case(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_test_flaskr_himtest_case(self):
+        self.fail()
